2b05de96416fde03f4b6f5e3d9e56104342f1a06
louaycodes <louay.zorai@esprit.tn>
2026-09-23 22:42:04 +0100

    Use fully qualified sonar-maven-plugin in runSonarAnalysis

--- vars/runSonarAnalysis.groovy
+++ vars/runSonarAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
     def projectKey = config.projectKey
     withSonarQubeEnv(config.sonarServer ?: 'SonarQube') {
-        sh "mvn -B clean verify sonar:sonar -Dsonar.projectKey=${projectKey}"
+        sh "mvn -B clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=${projectKey}"
     }
 }
