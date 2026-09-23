0f1ef380ae978bd4582a28161d5c0628e9691fe7
louaycodes <louay.zorai@esprit.tn>
2026-09-23 20:53:17 +0100

    Update default credentialsId to a49f0deb-93ab-4574-954f-3927d9f76b39

--- vars/dockerBuildAndPushFrontend.groovy
+++ vars/dockerBuildAndPushFrontend.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
     def imageName = config.imageName
     def dockerfilePath = config.dockerfilePath ?: 'docker/Dockerfile'
-    def credentialsId = config.credentialsId ?: 'docker-hub'
+    def credentialsId = config.credentialsId ?: 'a49f0deb-93ab-4574-954f-3927d9f76b39'
     def tag = config.tag ?: env.BUILD_NUMBER
 
     env.PATH = "/opt/homebrew/bin:${env.PATH}"
